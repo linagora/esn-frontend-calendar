f16d47658f0d7d816e37a67a5ab21ba1269e5c50
Huy Hoang TRINH <hhtrinh@linagora.com>
2020-07-10 15:46:58 +0700

    #2 - Cache Dynamic Templates

--- src/linagora.esn.calendar/app/run.js
+++ src/linagora.esn.calendar/app/run.js
@@ -12,5 +12,8 @@
   function addTemplateCache($templateCache) {
     $templateCache.put('/calendar/app/search/event/event-search-item.html', require('./search/event/event-search-item.pug'));
     $templateCache.put('/calendar/app/search/form/search-form-template.html', require('./search/form/search-form-template.pug'));
+    $templateCache.put('/calendar/app/components/entities-autocomplete-input/entities-autocomplete-input-tag.html', require('./components/entities-autocomplete-input/entities-autocomplete-input-tag.pug'));
+    $templateCache.put('/calendar/app/planning/aside/calendar-planning-for-aside.html', require('./planning/aside/calendar-planning-for-aside.pug'));
+    $templateCache.put('/calendar/app/sidebar/mobile/sidebar-mobile.html', require('./sidebar/mobile/sidebar-mobile.pug'));
   }
 })();
