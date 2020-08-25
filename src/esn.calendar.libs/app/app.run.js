d47b2e51d6b3c0cd3264cab6bf3d711078b86130
Huy TA QUOC <htquoc@linagora.com>
2020-08-25 14:33:07 +0700

    https://github.com/OpenPaaS-Suite/esn-frontend-calendar/issues/64: Moved registerTimeZones to the run block of 'esn.calendar.libs'

--- src/esn.calendar.libs/app/app.run.js
+++ src/esn.calendar.libs/app/app.run.js
@@ -1,7 +1,12 @@
 'use strict';
 
 angular.module('esn.calendar.libs')
+  .run(registerTimezones)
   .run(addTemplateCache);
+
+function registerTimezones(calRegisterTimezones) {
+  calRegisterTimezones();
+}
 
 function addTemplateCache($templateCache) {
   $templateCache.put('/calendar/app/components/entities-autocomplete-input/entities-autocomplete-input-tag.html', require('./components/entities-autocomplete-input/entities-autocomplete-input-tag.pug'));
