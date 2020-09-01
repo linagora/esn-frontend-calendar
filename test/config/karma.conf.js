9e5a49bb57c695d080e9f0ac691e04d6e99e4fdb
bspham <bspham@linagora.com>
2020-09-01 15:43:32 +0700

    OpenPaaS-Suite/esn#29 Setup CI and extend timeout for test

--- test/config/karma.conf.js
+++ test/config/karma.conf.js
@@ -4,8 +4,17 @@ const webpackConfig = require('../../webpack.test');
 
 module.exports = function(config) {
   const singleRun = process.env.SINGLE_RUN !== 'false';
+  // We need a timeout of at least 10000ms or else the tests will sometimes randomly
+  // fail because they exceed the default 2000ms timeout. This will happen often in
+  // the CI where the tests run slower than in our locals.
+  const timeout = process.env.TEST_TIMEOUT || 10000;
 
   config.set({
+    client: {
+      mocha: {
+        timeout
+      }
+    },
     basePath: '../../',
     files: [
       'src/index.test.js',
@@ -20,7 +29,11 @@ module.exports = function(config) {
     browsers: ['FirefoxHeadless'],
 
     customLaunchers: {
-      FirefoxHeadless: {base: 'Firefox', flags: ['--headless']}
+      FirefoxHeadless: {
+        base: 'Firefox',
+        flags: ['--headless'],
+        prefs: { 'network.proxy.type': 0 }
+      }
     },
 
     proxies: {
