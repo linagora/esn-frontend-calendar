71e73acf17436353d47ee1f4edb06012d44896a1
Huy TA QUOC <htquoc@linagora.com>
2020-09-25 11:38:33 +0700

    https://github.com/OpenPaaS-Suite/esn-frontend-calendar/issues/119: Added a test to make sure there is no missing timezone from now on

--- src/esn.calendar.libs/app/services/timezone.spec.js
+++ src/esn.calendar.libs/app/services/timezone.spec.js
@@ -1,20 +1,29 @@
 'use strict';
 
-/* global chai: false */
+/* global chai, _: false */
 
-var expect = chai.expect;
+const { expect } = chai;
 
 describe('The calRegisterTimezones factory', function() {
-  var ICAL, TIMEZONES;
+  let ICAL, TIMEZONES, ESN_DATETIME_TIMEZONE;
 
   beforeEach(function() {
     angular.mock.module('esn.calendar.libs');
-    angular.mock.inject(function(calRegisterTimezones, _ICAL_, _TIMEZONES_) {
+    angular.mock.inject(function(calRegisterTimezones, _ICAL_, _TIMEZONES_, _ESN_DATETIME_TIMEZONE_) {
       calRegisterTimezones();
       ICAL = _ICAL_;
       TIMEZONES = _TIMEZONES_;
+      ESN_DATETIME_TIMEZONE = _ESN_DATETIME_TIMEZONE_;
     });
 
+  });
+
+  it('should register all the timezones that are avaiable in the user configuration', function() {
+    const calendarTzList = Object.keys(TIMEZONES.zones).concat(Object.keys(TIMEZONES.aliases));
+    const esnUserConfigTzList = ESN_DATETIME_TIMEZONE.map(({ value }) => value);
+    const missingTzList = _.difference(esnUserConfigTzList, calendarTzList);
+
+    expect(missingTzList.length).to.equal(0);
   });
 
   it('should have register timezone', function() {
